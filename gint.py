b8f9ff6e4dd3cc790ce4b3f42660461f472177b9
Friedrich <frputtkammer@gmail.com>
2018-10-18 10:36:58 +0200

    added first error estimation method to ode solver

--- gint.py
+++ gint.py
@@ -1,7 +1,7 @@
 from gsolver import GaussianSolver
+import numpy as np
 
-
-def gaussian_integration(derivative, intial_values, time_interval, rel_tolerance):
+def gaussian_integration(derivative, initial_values, time_interval, rel_tolerance):
     """
         integrates given ode system using the gaussian method
         :param derivative: function, returns derivative for timepoint
@@ -9,4 +9,9 @@ def gaussian_integration(derivative, intial_values, time_interval, rel_tolerance
         :param initial_values: numpy array, array of initial values
     """
     solver = GaussianSolver(derivative, intial_values, time_interval, rel_tolerance)
-    solver.step(interval[0])
+    time_points = np.array(time_interval[0])
+    y_values = np.array(initial_values)
+    current_time_point = time_interval[0]
+    while current_time_point < time_interval[1]:
+        time_points = np.append(time_points, current_time_point)
+        y_values = np.append(y_values, solver.y)
--- gsolver.py
+++ gsolver.py
@@ -2,9 +2,8 @@ import numpy as np
 
 
 class GaussianSolver:
-    def __init__(self, derivative, initial_values, interval, rel_tolerance):
+    def __init__(self, derivative, initial_values, interval, rel_tolerance, error_estimation_method=1):
         """
-        creates new Solver object
         :param derivative: function, returns derivative for timepoint
         :param interval: tuple, start / end timepoint
         :param initial_values: numpy array, array of initial values
@@ -13,18 +12,38 @@ class GaussianSolver:
         self.y = initial_values
         self.max_time_step = interval[1] - interval[0]
         self.rtol = rel_tolerance
+        self.errEstMet = error_estimation_method
+
+    def estimate_error1(self, t, h):
+        """
+        :param t: float, current time point
+        :param h: float, time step
+        :return: error estimate, tolerance, current y change
+        """
+        dy_dt = self.derivative(self.y, t)
+        p = h * 10**(-15)
+        dyi = dy_dt * h
+        dyi_p = dy_dt * (h + p)
+        err = (dyi_p - dyi - dy_dt*p) / (2*h*p + p**2)
+        yi = self.y + dyi
+        tol = yi * 10**(-4)
+        return err, tol, dyi
 
     def step(self, time_point):
         """
-
+        does one integration step
         :param time_point: current timepoint
         :return:
         """
-        dy = self.derivative(self.y, time_point)
         err = np.inf
-        time_step = self.max_time_step / 100
+        time_step = self.max_time_step
         tol = 0
-        while err > tol:
-
+        dyi = 0
+        number_of_iterations = 0
+        while err > tol and number_of_iterations < 1000:
+            time_step = time_step / 100
+            err, tol, dyi = self.estimate_error1(time_point, time_step)
+            number_of_iterations += 1
+        self.y += dyi
         next_time_point = time_point + time_step
         return next_time_point
