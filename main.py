e2519c6d8d9150fe333b16021f8d1d20f0f56d2f
Friedrich <frputtkammer@gmail.com>
2018-10-19 16:50:38 +0200

    started improved compare function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.integrate import odeint
 from copy import deepcopy
 
+POLYNOMIAL_DEGREE = 8
+
 def derivative1(y, t):
     dy_dt = -y
     return dy_dt
@@ -22,7 +24,7 @@ def derivative3(n, t):
     change = np.array([dx_dt, dy_dt])
     return change
 
-
+'''
 def compare_solutions(ya, ta, yb, tb):
     mutual_time_points = []
     mutual_time_point_indices = []
@@ -38,13 +40,102 @@ def compare_solutions(ya, ta, yb, tb):
         ys = ya
     for i in range(0, len(tl)):
         for j in range(0, len(ts)):
+            print('tl',tl[i],'ts',ts[j])
             if tl[i] == ts[j]:
                 mutual_time_points.append(ts[i])
                 mutual_time_point_indices.append((j, i))
+                break
+            if ts[j] > tl[i]:
+                break
     y_value_differences = []
     for indexPair in mutual_time_point_indices:
         y_value_differences.append(yl[indexPair[0]] - ys[indexPair[1]])
     return mutual_time_points, y_value_differences
+'''
+
+
+def polynomial(t, coefficients_list):
+    y = 0
+    for i in range(len(coefficients_list)):
+        y += coefficients_list[i] * t ** i
+    return y
+
+
+def vectorial_field(t, list_of_coefficients_lists):
+    x = np.empty(list_of_coefficients_lists.shape[0])
+    for i in range(len(list_of_coefficients_lists)):
+        x[i] = polynomial(t, list_of_coefficients_lists[i])
+    return x
+
+
+def polynomial_fit(xa, ta, xb, tb):
+    if isinstance(xa.shape, tuple) and (xa.shape[1] == xb.shape[1]):
+        xa = np.transpose(xa)
+        xb = np.transpose(xb)
+        coefficients_a = np.empty((xa.shape[0], POLYNOMIAL_DEGREE+1))
+        coefficients_b = np.empty((xb.shape[0], POLYNOMIAL_DEGREE+1))
+        for variableIndex in range(xa.shape[0]):
+            coefficients_a[variableIndex] = np.polyfit(ta, xa[variableIndex], POLYNOMIAL_DEGREE)
+        li = []
+        for t in tb:
+            li.append(vectorial_field(t, coefficients_b)[1])
+        fig = plt.figure()
+        plot = fig.add_subplot(111)
+        plot.plot(tb, li)
+        plot.plot(tb, xb[1])
+        fig.savefig('bla.png')
+        for variableIndex in range(xb.shape[0]):
+            coefficients_b[variableIndex] = np.polyfit(tb, xb[variableIndex], POLYNOMIAL_DEGREE)
+        return coefficients_a, coefficients_b
+    elif isinstance(xa.shape, int) and (xa.shape[1] == xb.shape[1]):
+        coefficients_a = np.polyfit(ta, xa, POLYNOMIAL_DEGREE)
+        coefficients_b = np.polyfit(tb, xb, POLYNOMIAL_DEGREE)
+        return coefficients_a, coefficients_b
+    else:
+        raise Exception('ndarrays have different second dimensions:'+str(xa.shape)+' and '+str(xb.shape))
+
+
+def compare_solutions(xa, ta, xb, tb, tm):
+    # try:
+    coefficients_a, coefficients_b = polynomial_fit(xa, ta, xb, tb)
+    time_point_number = len(tm)
+    if isinstance(xa.shape, tuple):
+        variable_number = coefficients_a.shape[0]
+        difference = np.empty((time_point_number, variable_number))
+        for t_index in range(time_point_number):
+                t = tm[t_index]
+                difference[t_index] = vectorial_field(t, coefficients_a) - vectorial_field(t, coefficients_b)
+        return difference
+    elif isinstance(xa.shape, int):
+        difference = np.empty(time_point_number)
+        for t_index in range(time_point_number):
+            t = tm[t_index]
+            difference[t_index] = polynomial(t, coefficients_a) - polynomial(t, coefficients_b)
+        return difference
+    '''
+    except Exception as e:
+        print(e)
+        return 'ERROR'
+    '''
+
+'''
+def reshape_acc_variables(array):
+    """
+    reorders array vector v(t) into arrays v1(t), v2(t) etc.
+    :param array: numpy array, representing vector in time course
+    :return: numpy array, reshaped
+    """
+    num_var = array.shape[1] # number of variables
+    len_rs = array.shape[0] / num_var # length of reshaped array
+    reshaped_array = np.empty((num_var, len_rs))
+    for variable_index in range(num_var):
+        for time_point in range(len_rs):
+            var_arr = reshaped_array[variable_index] # values for time point for variable index
+            current_index = time_point * num_var + variable_index  # index of time point in flattened array
+            var_arr = np.append(var_arr, array.flat[current_index])
+    return reshaped_array
+'''
+
 
 initialValues = np.array([10.0, 10.0])
 initialValues2 = deepcopy(initialValues)
@@ -79,7 +170,7 @@ plot.plot(timePoints2, y2, label='x odeint')
 plot.plot(timePoints2, x2, label='y odeint')
 plot.plot(timePoints3, y3, label='x low tolerance')
 plot.plot(timePoints3, x3, label='y low tolerance')
-
+# vgl mit odeint:
 xDiff = []
 yDiff = []
 for i in range(0, len(yValues3)):
@@ -88,10 +179,13 @@ for i in range(0, len(yValues3)):
 plot.plot(timePoints3, yDiff, label='y diff')
 plot.plot(timePoints3, xDiff, label='x diff')
 '''
-
-for
 xDiff = []
 yDiff = []
+difference = compare_solutions(yValues, timePoints, yValues3, timePoints3, timePoints)
+for x in difference:
+    xDiff.append(x[0])
+    yDiff.append(x[1])
+plot.plot(timePoints, yDiff, label='y diff')
+plot.plot(timePoints, xDiff, label='x diff')
 fig.legend()
-
 plt.show()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-y = [1, 3, 4, 5]
-x = y[0: len(y): 2]
-print(x)
+import numpy as np
+
+
+
