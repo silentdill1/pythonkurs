1ba49d46f264aaeba5455b4882a9bdff5801056d
Friedrich <frputtkammer@gmail.com>
2018-10-26 16:19:36 +0200

    added AA specific abundance tracing

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-import numpy as np
-
-
+def create_polynom(coefficients_list):
+    def poly(t):
+        value = 0
+        for i in range(len(coefficients_list)):
+            value += t**i * coefficients_list[i]
+        return value
+    return poly
 
