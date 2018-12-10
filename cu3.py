60475bc4e9a9dc6c1cc13b5ad2242f99570b9fb3
Friedrich <frputtkammer@gmail.com>
2018-12-10 11:20:26 +0100

    course exercises 3 and 4

---
+++ cu3.py
@@ -0,0 +1,67 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+
+def hd(k_mer, text):
+    k = len(k_mer)
+    h_dis = k
+    for i in range(len(text)):
+        n_hd = k
+        for j, letter in enumerate(text[i:i+k]):
+            if letter == k_mer[j]:
+                n_hd -= 1
+        if n_hd < h_dis:
+            h_dis = n_hd
+    return h_dis
+
+
+def frequent_words(k, d, text):
+    k_mers = {}
+    for i in range(len(text)-k):
+        current_k_mer = text[i:i+k]
+        k_mers[current_k_mer] = -1
+        for j in range(len(text)-k):
+            if hd(current_k_mer, text[j:j+k]) <= d:
+                k_mers[current_k_mer] += 1
+    max_num = max(k_mers.items(), key=(lambda v: v[1]))[1]
+    most_freq = {}
+    for key, value in k_mers.items():
+        if value == max_num:
+            most_freq[key] = value
+    return most_freq
+
+
+def long_string(a):
+    base_string = 'ACGTTGCATGTCGCATGATGCATGATTTCCCATTATATATTCGTAGTCTGATCTGATCG'
+    return base_string*a
+
+
+def neighbors(string, d):
+    print(string)
+    if d == 0:
+        return [string]
+    if len(string) == 1:
+        return ['A', 'C', 'G', 'T']
+    else:
+        neighbors_list = []
+        for i in range(len(string)):
+            for neighbor in neighbors(string[i+1:], d-1):
+                neighbors_list.extend([string[:i]+'A'+neighbor, string[:i]+'G'+neighbor, string[:i]+'T'+neighbor,
+                                       string[:i]+'C'+neighbor])
+        return neighbors_list
+
+
+'''
+li = []
+for a in range(1, 15):
+    start = time.time()
+    print(frequent_words(4, 1, long_string(a)))
+    end = time.time()
+    li.append(end-start)
+plt.plot(range(1, 15), li)
+plt.show()
+'''
+if __name__ == "__main__":
+    print(neighbors('AGT', 2))
+
---
+++ cu4.py
@@ -0,0 +1,71 @@
+import numpy as np
+from cu3 import frequent_words
+import time
+import matplotlib.pyplot as plt
+
+SYM_TO_NUM = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+NUM_TO_SYM = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
+
+
+def pattern_to_number(pattern):
+    if len(pattern) == 0:
+        return 0
+    else:
+        symbol = pattern[-1]
+        prefix = pattern[:-1]
+        return 4*pattern_to_number(prefix)+SYM_TO_NUM[symbol]
+
+
+def number_to_pattern(index, k):
+    if k == 1:
+        return NUM_TO_SYM[index]
+    else:
+        pref_ind = index // 4
+        r = index % 4
+        symb = NUM_TO_SYM[r]
+        pref_pattern = number_to_pattern(pref_ind, k-1)
+        return pref_pattern+symb
+
+
+def computing_frequencies(text, k):
+    frequency_array = np.zeros(4**k)
+    for start_index in range(len(text)-k):
+        k_mer = text[start_index:start_index+k]
+        index = pattern_to_number(k_mer)
+        frequency_array[index] += 1
+    return frequency_array
+
+
+def faster_frequent_words(text, k):
+    frequency_array = computing_frequencies(text, k)
+    max_number = max(frequency_array)
+    frequent_k_mers = []
+    for i in range(len(frequency_array)):
+        if frequency_array[i] == max_number:
+            pattern = number_to_pattern(i, k)
+            frequent_k_mers.append(pattern)
+    return frequent_k_mers
+
+
+def long_string(a):
+    base_string = 'ACGTTGCATGTCGCATGATGCATGATTTCCCATTATATATTCGTAGTCTGATCTGATCG'
+    return base_string*a
+
+
+li = []
+li2 = []
+'''
+for a in range(1, 15):
+    start = time.time()
+    print(frequent_words(4, 0, long_string(a)))
+    end = time.time()
+    li.append(end-start)
+'''
+for a in range(1, 1000):
+    start = time.time()
+    faster_frequent_words(long_string(a), 4)
+    end = time.time()
+    li2.append(end-start)
+# plt.plot(range(1, 15), li)
+plt.plot(range(1, 1000), li2)
+plt.show()
